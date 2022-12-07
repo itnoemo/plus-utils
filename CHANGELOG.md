4053961601c66273449cbede9a394bdd1b58ba06
yincece <ycc0316@126.com>
2022-12-07 20:44:02 +0800

    feat: 新增commit校验

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# [1.0.0](https://github.com/weixiaodece/plus-utils/compare/v1.0.2...v1.0.0) (2022-12-07)
+
+
+
+## 1.0.2 (2022-12-05)
+
+
+
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 export default {
-    testRegex: "(/test/__tests__/.*|(\\.|/)(test|spec))\\.js?$",
+    verbose: true,  // 层次显示测试套件中每个测试的结果。
+    bail: false, // 是否显示日志信息
+    testRegex: "(/test/__tests__/.*|(\\.|/)(test|spec))\\.js?$", // 测试用例目录
     moduleFileExtensions: ['js', 'ts', 'tsx'],
     testPathIgnorePatterns: ['/node_modules/'],
     transform: {
